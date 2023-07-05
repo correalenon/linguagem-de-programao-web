05cc5966099fb955354bb592c3677fad0e5f1683
Lenon Corrêa <lenonac_@hotmail.com>
2023-07-05 00:27:47 -0300

    Ajustes na classe Administrador, referentes a SQL

--- protected/administrador.php
+++ protected/administrador.php
@@ -12,7 +12,49 @@
 
         public function getStudents()
         {
-            $query = "SELECT nome, email, matricula, datanascimento FROM aluno";
+            $query = "SELECT id, nome, email, matricula, datanascimento FROM aluno";
+            $sql = mysqli_query($this->connection, $query);
+            if (!$sql) {
+                die("Falha na consulta ao banco.");
+            }
+            if (empty($sql)) {
+                return False;
+            } else {
+                return $sql;
+            }
+        }
+
+        public function createStudent(string $nome, string $email, string $matricula, string $datanascimento)
+        {
+            $query = "INSERT INTO aluno (nome, email, matricula, datanascimento) VALUES ('$nome', '$email', '$matricula', '$datanascimento')";
+            $sql = mysqli_query($this->connection, $query);
+            if (!$sql) {
+                die("Falha na consulta ao banco.");
+            }
+            if (empty($sql)) {
+                return False;
+            } else {
+                return $sql;
+            }
+        }
+
+        public function updateStudent(string $id, string $nome, string $email, string $matricula, string $datanascimento)
+        {
+            $query = "UPDATE aluno SET nome = '$nome', email = '$email', matricula = '$matricula', datanascimento = '$datanascimento' WHERE id = '$id'";
+            $sql = mysqli_query($this->connection, $query);
+            if (!$sql) {
+                die("Falha na consulta ao banco.");
+            }
+            if (empty($sql)) {
+                return False;
+            } else {
+                return $sql;
+            }
+        }
+
+        public function deleteStudent(string $id)
+        {
+            $query = "DELETE FROM aluno WHERE id = '$id'";
             $sql = mysqli_query($this->connection, $query);
             if (!$sql) {
                 die("Falha na consulta ao banco.");
@@ -26,7 +68,36 @@
 
         public function getTeachers()
         {
-            $query = "SELECT nome, email, datanascimento FROM professor";
+            $query = "SELECT id, nome, email, datanascimento FROM professor";
+            $sql = mysqli_query($this->connection, $query);
+            if (!$sql) {
+                die("Falha na consulta ao banco.");
+            }
+            if (empty($sql)) {
+                return False;
+            } else {
+                return $sql;
+            }
+        }
+
+        public function updateTeacher(string $id, string $nome, string $email, string $datanascimento)
+        {
+            $query = "UPDATE professor SET nome = '$nome', email = '$email', datanascimento = '$datanascimento' WHERE id = '$id'";
+            $sql = mysqli_query($this->connection, $query);
+            if (!$sql) {
+                die("Falha na consulta ao banco.");
+            }
+            if (empty($sql)) {
+                return False;
+            } else {
+                return $sql;
+            }
+        }
+
+        public function deleteTeacher(string $id)
+        {
+            print($id);
+            $query = "DELETE FROM professor WHERE id = '$id'";
             $sql = mysqli_query($this->connection, $query);
             if (!$sql) {
                 die("Falha na consulta ao banco.");
@@ -40,10 +111,40 @@
 
         public function getNotes()
         {
-            $query = "SELECT A.nome, D.disciplina, N.nota FROM notas N
+            $query = "SELECT N.id, A.nome AS aluno, D.disciplina, N.nota, P.nome as professor FROM notas N
                         JOIN disciplinas_cursadas DC ON DC.id_disciplina = N.id_disciplina_cursada 
                         JOIN disciplinas D ON D.id  = DC.id_disciplina 
-                        JOIN aluno A ON A.id = DC.id_aluno ";
+                        JOIN disciplinas_ministradas DM ON DM.id_disciplina = D.id
+                        JOIN professor P ON P.id = DM.id_professor
+                        JOIN aluno A ON A.id = DC.id_aluno";
+            $sql = mysqli_query($this->connection, $query);
+            if (!$sql) {
+                die("Falha na consulta ao banco.");
+            }
+            if (empty($sql)) {
+                return False;
+            } else {
+                return $sql;
+            }
+        }
+
+        public function updateNote(string $id, string $nota)
+        {
+            $query = "UPDATE notas SET nota = '$nota' WHERE id = '$id'";
+            $sql = mysqli_query($this->connection, $query);
+            if (!$sql) {
+                die("Falha na consulta ao banco.");
+            }
+            if (empty($sql)) {
+                return False;
+            } else {
+                return $sql;
+            }
+        }
+
+        public function deleteNote(string $id)
+        {
+            $query = "DELETE FROM notas WHERE id = '$id'";
             $sql = mysqli_query($this->connection, $query);
             if (!$sql) {
                 die("Falha na consulta ao banco.");
